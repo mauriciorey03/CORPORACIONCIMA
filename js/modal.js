783a27f837af97891c9ed73690e86d33ba92f0c3
mauriciorey03 <edgarmeza909@gmail.com>
2024-12-13 05:57:20 -0500

    Alerta de actualización v2

--- js/modal.js
+++ js/modal.js
@@ -1,26 +1,27 @@
-    // Función que crea y muestra el banner
-    function showConstructionBanner() {
-      const banner = document.createElement('div');
-      banner.className = 'construction-banner';
-      banner.innerHTML = `
-        <i class="fas fa-times close-banner"></i>
-        <div class="construction-icon">
-          <i class="fas fa-tools"></i>
-          <span class="construction-title">Sitio en Construcción</span>
-        </div>
-        <p class="construction-text">Estamos trabajando para mejorar nuestro sitio web. Algunas funcionalidades podrían no estar disponibles.</p>
-        <p class="contact-info">Consultas: (+57) 607 670 4695 | gerencia@corporacion-cima.com</p>
-      `;
+// Función que crea y muestra el banner
+function showConstructionBanner() {
+  const banner = document.createElement('div');
+  banner.className = 'construction-banner';
+  banner.innerHTML = `
+    <i class="fas fa-times close-banner"></i>
+    <div class="construction-icon">
+      <i class="fas fa-tools"></i>
+      <span class="construction-title">Sitio web en Construcción</span>
+    </div>
+    <p class="construction-text">Estamos trabajando en nuestro sitio web. Algunas funcionalidades podrían estar en proceso de actualización o no estar disponibles.</p>
+    <p class="contact-info">Consultas: (+57) 607 670 4695 </p>
+    <p class="contact-info"> contacto@corporacioncima.com</p>
+  `;
 
-      document.body.appendChild(banner);
+  document.body.appendChild(banner);
 
-      banner.querySelector('.close-banner').addEventListener('click', () => {
-        banner.classList.add('sliding-out');
-        setTimeout(() => {
-          banner.remove();
-        }, 500);
-      });
-    }
+  banner.querySelector('.close-banner').addEventListener('click', () => {
+    banner.classList.add('sliding-out');
+    setTimeout(() => {
+      banner.remove();
+    }, 500);
+  });
+}
 
-    // Mostrar el banner cuando la página se carga
-    document.addEventListener('DOMContentLoaded', showConstructionBanner);
+// Mostrar el banner cuando la página se carga
+document.addEventListener('DOMContentLoaded', showConstructionBanner);
